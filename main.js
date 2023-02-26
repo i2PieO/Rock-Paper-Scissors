ffe765256bb1ba6611f44ce2d684fd9521cfe5eb
Syed Jawad <jawadsyed406@gmail.com>
2023-02-26 20:52:42 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,74 @@
+const choices = ["rock","paper","scissors"]
+
+function game() {
+    for(let i = 1; i<=5; i++) {
+        playRound();
+    }
+    logWins();
+}
+
+function playRound() {
+    const playerSelection = playerChoice();
+    const computerSelection = computerChoice();
+    const winner = checkWinner(playerSelection, computerSelection);
+    winners.push(winner);
+}
+
+function playerChoice () {
+    let input = prompt("Type Rock, Paper, or Scissors");
+    while (input == null) {
+        input = prompt("Type Rock, Paper, or Scissors");
+    }
+    input = input.toLowerCase();
+    let check = validateInput(input);
+    while (check == false) {
+        input = prompt(
+            "Type Rock, Paper, or Scissors, Spelling needs to be exact, but capitilization doesnt matter"
+        );
+        while (input == null) {
+            input = prompt("Type Rock, Paper, or Scissors");
+        }
+        input = input.toLowerCase();
+        check = validateInput(input);
+    }
+    return input;
+}
+
+function computerChoice(){
+    return choices[Math,floor(Math.random()*choices.length)]
+}
+
+function validateInput(choice) {
+    return choices.includes(choice);
+}
+
+function checkWinner(choiceP, choiceC) {
+    console.log(choiceP, choiceC);
+    if (choiceP === choiceC) {
+        return "Tie";
+    } else if (
+        (choiceP === "rock" && choiceC == "scissors") ||
+        (choiceP === "paper" && choiceC == "rock") ||
+        (choiceP === "scissors" && choiceC == "paper")
+     ) {
+        return "Player";
+    } else {
+        return "Computer";
+    }
+}
+
+function logWins() {
+    let playerWins = winners.filter((item) => item == "Player").length;
+    let computerWins = winners.filter((item) => item == "Computer").length;
+    let ties = winners.filter((item) => item == "Tie").length;
+    console.log("Results:");
+    console.log("Player Wins:", playerWins);
+    console.log("Computer Wins:", computerWins);
+    console.log("Ties:". ties);
+}
+
+function logRound(playerChoice,computerChoice,winner){
+    
+}
+
+game();
